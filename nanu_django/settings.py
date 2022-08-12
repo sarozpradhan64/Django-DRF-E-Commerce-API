41d2ee7b5cf88e6a23f2e4fcced85ebc276145f3
sarozpradhan64 <sarozpradhan64@gmail.com>
2022-08-12 15:58:56 +0545

    static dir added

--- nanu_django/settings.py
+++ nanu_django/settings.py
@@ -129,6 +129,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+# added
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_ROOT = '/home6/gyannhub/public_html/nanu'
 
