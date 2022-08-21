934ee0f794dca20de829fc65abbf358f11de35f0
sarozpradhan64 <sarozpradhan64@gmail.com>
2022-08-21 11:32:11 +0545

    url fix

--- main/serializers.py
+++ main/serializers.py
@@ -8,12 +8,12 @@ from .models import Product, Product_category, Carousel, Product_image
 
 
 class ProductSerializers(serializers.ModelSerializer):
-    thumbnail = serializers.ImageField(max_length=None, use_url=True, allow_null=True, required=False)
     class Meta:
         model= Product
         exclude = ['cost_price',]
         lookup_field = 'slug'
 
+# serializing category
 class ProductCategorySerializers(serializers.ModelSerializer):
     class Meta:
         model= Product_category
--- nanu_django/urls.py
+++ nanu_django/urls.py
@@ -13,14 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from argparse import Namespace
+
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 urlpatterns = [
      path('api/', include('main.urls')),
-    path('', admin.site.urls)
+    path('admin', admin.site.urls)
    
 ]
 
